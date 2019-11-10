ea7c3227139e069a95fe4c8844072e6b5b526f5b
David Anthoff <anthoff@berkeley.edu>
2019-11-10 12:09:36 -0800

    Add PR creation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as github from'@actions/github';
 
 async function run() {
   try {
     const GITHUB_TOKEN = core.getInput('github-token', { required: true });
     const REMOTE_REPO = `https://${process.env.GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
 
+    const octokit = new github.GitHub(GITHUB_TOKEN);
+
     await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add(PackageSpec(url="https://github.com/davidanthoff/PkgButler.jl", rev="master"))']);
 
-    await exec.exec('git', ['checkout', '-b', 'julia-butler-branch'])
+    await exec.exec('git', ['branch', '-D', 'julia-pkgbutler-updates'])
+    await exec.exec('git', ['checkout', '-b', 'julia-pkgbutler-updates'])
 
     await exec.exec('julia', ['--color=yes', '-e', 'import PkgButler; PkgButler.update_pkg(pwd())']);
     
@@ -24,7 +28,14 @@ async function run() {
 
       await exec.exec('git', ['remote', 'add', 'publisher', REMOTE_REPO]);
 
-      await exec.exec('git', ['push', 'publisher', 'julia-butler-branch'])
+      await exec.exec('git', ['push', '-f', 'publisher', 'julia-pkgbutler-updates'])
+
+      octokit.pulls.create({
+        ...github.context.repo,
+        title: 'Julia Package Butler Updates',
+        head: 'julia-pkgbutler-updates',
+        base: 'master'
+      })
     }
 
   } catch (error) {
