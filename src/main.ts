7be877dee1afbce2d7214832a9b579c30786f6fe
David Anthoff <anthoff@berkeley.edu>
2019-11-09 20:12:38 -0800

    Updates

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,24 @@ import * as exec from '@actions/exec';
 
 async function run() {
   try {
-    await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add(PackageSpec(url="https://github.com/davidanthoff/PkgButler.jl", rev="master"))'])
-    await exec.exec('julia', ['--color=yes', '-e', 'import PkgButler; PkgButler.update_pkg(pwd())'])
+    await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add(PackageSpec(url="https://github.com/davidanthoff/PkgButler.jl", rev="master"))']);
+
+    await exec.exec('git', ['checkout', '-b', 'julia-butler-branch'])
+
+    await exec.exec('julia', ['--color=yes', '-e', 'import PkgButler; PkgButler.update_pkg(pwd())']);
     
+    try {
+      await exec.exec('git', ['diff-index', '--quiet', 'HEAD', '--']);
+    }
+    catch (error) {
+      
+      await exec.exec('git', ['add', '.'])
+
+      await exec.exec('git', ['commit', '-m', '"Julia butler updates"'])
+
+      await exec.exec('git', ['push', 'origin', 'julia-butler-branch'])
+    }
+
   } catch (error) {
     core.setFailed(error.message);
   }
